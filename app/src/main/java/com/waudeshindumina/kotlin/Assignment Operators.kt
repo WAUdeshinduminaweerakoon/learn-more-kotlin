abe2f0ab258628b7c28d2b899a2b8bb18d2d02fb
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-07-23 21:29:50 +0530

    Assignment Operators and Increment Operators study

---
+++ app/src/main/java/com/waudeshindumina/kotlin/Assignment Operators.kt
@@ -0,0 +1,51 @@
+package com.waudeshindumina.kotlin
+
+//Assignment Operators
+//(+=, -=, *=, /=, %=)
+fun main() {
+    var num = 8
+//    num = num + 4
+//    println(num)//12
+    num += 4
+    println(num)//12
+
+
+    var num1 = 8
+    //    num1 = num1 - 4
+//    println(num)//4
+    num1 -= 4
+    println(num1)//4
+
+
+    var num2 = 8
+//    num2 = num2 * 4
+//    println(num)//32
+    num2 *= 4
+    println(num2)//32
+
+    var num3 = 9
+//    num3 = num3 / 4
+//    println(num)//2
+    num3 /= 4
+    println(num3)//4
+
+    var num4 = 11
+//    num4 = num4 % 4
+//    println(num4)//3
+    num4 %= 4
+    println(num4)//3
+
+    //Increment Operators (++ , --)
+    var num5 = 5
+    //num5 = num5 + 1//6
+    num5++//6
+    println(num5)
+
+    var num6 = 5
+    //num6 = num6 - 1//4
+    num6--//4
+    println(num6)
+
+
+
+}
